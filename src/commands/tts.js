663d70e73505617f9e4f5ec63cb0ddebb6d26684
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-02-12 01:22:38 -0300

    Create tts.js

---
+++ src/commands/tts.js
@@ -0,0 +1,47 @@
+import { getAudioBuffer } from "simple-tts-mp3";
+
+export default {
+  name: "tts",
+  description: "Convierte texto a voz con el texto que proporciones.",
+  alias: [],
+  use: "!tts 'texto'",
+
+  run: async (socket, msg, args) => {
+    try {
+      const content = args.join(" ");
+
+      if (!content) {
+        socket.sendMessage(msg.messages[0].key.remoteJid, {
+          text: "Ingresa un texto para convertir a voz.",
+        });
+
+        return;
+      }
+
+      socket.sendMessage(msg.messages[0].key.remoteJid, {
+        react: { text: "🕑", key: msg.messages[0]?.key },
+      });
+
+      const tts = await getAudioBuffer(content, "es");
+
+      await socket.sendMessage(msg.messages[0].key.remoteJid, {
+        audio: tts,
+        mimetype: "audio/mp4",
+      });
+
+      socket.sendMessage(msg.messages[0].key.remoteJid, {
+        react: { text: "✅", key: msg.messages[0]?.key },
+      });
+    } catch (error) {
+      console.error(error);
+
+      await socket.sendMessage(msg.messages[0].key.remoteJid, {
+        text: "Ha ocurrido un error inesperado.",
+      });
+
+      socket.sendMessage(msg.messages[0].key.remoteJid, {
+        react: { text: "❌", key: msg.messages[0]?.key },
+      });
+    }
+  },
+};
